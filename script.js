682a5a73a3c0adba1b8770435600ecc6e3657a05
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-03-07 12:54:14 +0100

    Added languages to project cards. CSS and JS.

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ const projects = [
         title: "Freeplay Haven",
         description: "Finn spill som er gratis ved hjelp av API, med mulighet for sortering av kategorier, platformer, relevans, alfabetisk, popularitet og utgivelse dato. Inkluderer paginering.",
         imageSrc: "./images/project-images/freeplay-haven.jpg",
-        languages: ["html", "css", "javascript"]
+        languages: ["HTML", "CSS", "JavaScript"]
     },
     {
         title: "CubeDash",
         description: "Et spill hvor man beveger en kube for å unngå fallende kuber. Inkluderer vanskelighets-grad, score og high-score.",
         imageSrc: "./images/project-images/cubedash.jpg",
-        languages: ["html", "css", "javascript"]
+        languages: ["HTML", "CSS", "JavaScript"]
     },
     {
         title: "The Royal Edict of To-do's",
         description: "En oppgave liste hvor brukeren kan legge til oppgaver med en mulighet for manuell sortering, nummerering, avkrysning, skjuling og sletting av fullførte oppgaver.",
         imageSrc: "./images/project-images/todo-list.jpg",
-        languages: ["html", "css", "javascript"]
+        languages: ["HTML", "CSS", "JavaScript"]
     },
     {
         title: "Drumkit",
         description: "Vestibulum interdum, dolor sit amet gravida pulvinar, ipsum massa mattis turpis, ut dictum ante tellus quis ex. Quisque aliquet non tellus sodales consequat. Cras sapien ipsum, luctus at scelerisque rutrum, luctus non dui. Maecenas vitae ex at nunc sodales scelerisque.",
         imageSrc: "./images/project-images/drumkit.jpg",
-        languages: ["html", "css", "javascript"]
+        languages: ["HTML", "CSS", "JavaScript"]
     },
 ]
 
@@ -33,7 +33,7 @@ const projects = [
 generateProjectCards();
 let isScrolling = false;
 updateActiveNavItem("hero")
-handleScroll("hero");
+// handleScroll("hero");
 
 
 navItems.forEach(item => {
@@ -69,6 +69,14 @@ function generateProjectCards() {
     const title = document.createElement("h3");
     const description = document.createElement("p");
     const image = document.createElement("img");
+    const languageContainer = document.createElement("div");
+
+    e.languages.forEach(lang => {
+        const languageItem = document.createElement("p");
+        languageItem.textContent = lang;
+        languageItem.classList.add("card-language-item");
+        languageContainer.append(languageItem);
+    })
 
     title.textContent = e.title;
     description.textContent = e.description;
@@ -79,9 +87,10 @@ function generateProjectCards() {
     title.classList.add("card-title");
     description.classList.add("card-description");
     image.classList.add("project-image");
+    languageContainer.classList.add("card-language-container");
 
     textContainer.append(title, description)
-    cardContainer.append(image, textContainer);
+    cardContainer.append(image, textContainer, languageContainer);
     projectsWrapper.append(cardContainer);
     })
 }
